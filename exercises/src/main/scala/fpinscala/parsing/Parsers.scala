f44271667c91da8ed1903fbd15fcd63f4dc367ec
Feynman Liang <fliang@databricks.com>
2015-07-20 16:46:42 -0700

    Add mapLaw for prop testing

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -26,6 +26,11 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
   }
 
   object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      forAll(in)(s => self.run(p1)(s) == self.run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(a => a))(in)
   }
 }
 
