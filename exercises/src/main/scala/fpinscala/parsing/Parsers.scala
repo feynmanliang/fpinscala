f0a31df1eada8c8e00b687b5c63062d6d813c9e9
Feynman Liang <fliang@databricks.com>
2015-07-20 17:03:21 -0700

    Add map2 combinator and many1

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -15,7 +15,13 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
 
   def many[A](p: Parser[A]): Parser[List[A]]
+  def many1[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p))(_ :: _)
   def map[A,B](p: Parser[A])(f: A => B): Parser[B]
+  def map2[A,B,C](p1: Parser[A], p2: Parser[B])(f: (A,B) => C): Parser[C] =
+    p1 product(p2) map(f.tupled)
+  def product[A,B](p1: Parser[A], p2: Parser[B]): Parser[(A,B)]
+  def slice[A](p: Parser[A]): Parser[String]
 
   // parses a single character
   def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
@@ -29,6 +35,8 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
     def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p,p2)
     def many: Parser[List[A]] = self.many(p)
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def **[B](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
+    def product[B](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
   }
 
   object Laws {
