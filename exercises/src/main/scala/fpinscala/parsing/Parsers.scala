8a54fb71155462b63d3e2a01f089ad62e0dc770c
Feynman Liang <fliang@databricks.com>
2015-07-20 20:27:05 -0700

    Add JSON parser

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -11,13 +11,12 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
   implicit def regex(r: Regex): Parser[String]
 
-  /*
-   Primitive Combinators
-   */
+  /** Primitive Combinators */
   implicit def string(s: String): Parser[String] // implicit conversion from string to Parser[String]
 
   def flatMap[A,B](p: Parser[A])(f: A => Parser[B]): Parser[B]
 
+  // Returns the string of input parsed by parser, ignoring parser output
   def slice[A](p: Parser[A]): Parser[String]
 
   // unit, lifts a value A into a Parser[A]
@@ -37,9 +36,10 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
   // delays committing to a parse
   def attempt[A](p: Parser[A]): Parser[A]
 
-  /*
-   Derived Combinators
-   */
+  /** Derived Combinators */
+ // Equivalence of flatMap <=> seq
+  def seq[U,A,B](f: U => Parser[A])(g: A => Parser[B]): U => Parser[B] = u => flatMap(f(u))(g)
+
   //def product[A,B](p1: Parser[A], p2: => Parser[B]): Parser[(A,B)] =
   //  p1.flatMap(a => p2.flatMap(b => succeed((a,b))))
   def product[A,B](p1: Parser[A], p2: => Parser[B]): Parser[(A,B)] = for {
@@ -70,6 +70,65 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
   // parses a single character
   def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
 
+  /** Sequences two parsers, ignoring the result of the first.
+    * We wrap the ignored half in slice, since we don't care about its result. */
+  def skipL[B](p: Parser[Any], p2: => Parser[B]): Parser[B] =
+    map2(slice(p), p2)((_,b) => b)
+
+  /** Sequences two parsers, ignoring the result of the second.
+    * We wrap the ignored half in slice, since we don't care about its result. */
+  def skipR[A](p: Parser[A], p2: => Parser[Any]): Parser[A] =
+    map2(p, slice(p2))((a,b) => a)
+
+  /** Parser which consumes zero or more whitespace characters. */
+  def whitespace: Parser[String] = "\\s*".r
+
+  /** Parser which consumes reluctantly until it encounters the given string. */
+  def thru(s: String): Parser[String] = (".*?"+Pattern.quote(s)).r
+
+  /** Unescaped string literals, like "foo" or "bar". */
+  //def quoted: Parser[String] = string("\"") *> thru("\"").map(_.dropRight(1))
+  def quoted: Parser[String] = surround("\"", "\"")(".*?".r)
+
+  /** Unescaped or escaped string literals, like "An \n important \"Quotation\"" or "bar". */
+  def escapedQuoted: Parser[String] =
+    // rather annoying to write, left as an exercise
+    // we'll just use quoted (unescaped literals) for now
+    token(quoted label "string literal")
+
+  /** C/Java style floating point literals, e.g .1, -1.0, 1e9, 1E-23, etc.
+    * Result is left as a string to keep full precision
+    */
+  def doubleString: Parser[String] =
+    token("[-+]?([0-9]*\\.)?[0-9]+([eE][-+]?[0-9]+)?".r)
+
+  /** Floating point literals, converted to a `Double`. */
+  def double: Parser[Double] =
+    doubleString map (_.toDouble) label "double literal"
+
+  /** Parses a token and strips whitespace */
+  def token[A](p: Parser[A]): Parser[A] = attempt(p) <* whitespace
+
+  /** Zero or more repetitions of `p`, separated by `p2`, whose results are ignored. */
+  def sep[A](p: Parser[A], p2: Parser[Any]): Parser[List[A]] = // use `Parser[Any]` since don't care about result type of separator
+    sep1(p,p2) or succeed(List())
+
+  /** One or more repetitions of `p`, separated by `p2`, whose results are ignored. */
+  def sep1[A](p: Parser[A], p2: Parser[Any]): Parser[List[A]] =
+    map2(p, many(p2 *> p))(_ :: _)
+
+  /** Wraps `p` in start/stop delimiters. */
+  def surround[A](start: Parser[Any], stop: Parser[Any])(p: => Parser[A]) =
+    start *> p <* stop
+
+  /** A parser that succeeds when given empty input. */
+  def eof: Parser[String] =
+    regex("\\z".r).label("unexpected trailing characters")
+
+  /** The root of the grammar, expects no further input following `p`. */
+  def root[A](p: Parser[A]): Parser[A] =
+    p <* eof
+
   case class ParserOps[A](p: Parser[A]) {
     def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p,p2)
     def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p,p2)
@@ -78,6 +137,13 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
     def **[B](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
     def product[B](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
     def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
+    def *>[B](p2: => Parser[B]) = self.skipL(p, p2)
+    def <*(p2: => Parser[Any]) = self.skipR(p, p2)
+    def sep[B](p2: => Parser[B]) = self.sep(p, p2)
+    def token = self.token(p)
+    def as[B](b: B): Parser[B] = self.map(p)(_ => b)
+    def scope(msg: String) = self.scope(msg)(p)
+    def label(msg: String) = self.label(msg)(p)
   }
 
   object Laws {
